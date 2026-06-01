66a364b7363f56472a408e32a746b1e38ac25a80
Leonardo Vreka <leonardovreka@gmail.com>
2026-06-01 21:32:36 +0300

    set up of minio + creating posts model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    title = models.CharField(max_length=255)
+    image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username} - {self.title}"
